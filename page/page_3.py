a69f6fbf9a26522d38757e1d79e0b1b77d783f58
W-DataScientist <165705997+W-DataScientist@users.noreply.github.com>
2025-01-14 20:22:18 -0300

    Update page_3.py

--- page/page_3.py
+++ page/page_3.py
@@ -5,8 +5,16 @@ import pandas as pd  # Importa pandas para manipulação de dados em forma de Da
 def main():
     # Define o título da página
     st.title("Página 3")
+    
     # Adiciona uma descrição da página
     st.write("Esta é a terceira página do aplicativo.")
+    
+    # Texto explicativo sobre a Página 3
+    st.write("""
+        Nesta página, você encontrará uma tabela que apresenta dados organizados em colunas.
+        Os dados podem ser utilizados para análises e visualizações mais detalhadas.
+        Sinta-se à vontade para modificar os dados ou adicionar novas colunas conforme necessário.
+    """)
     
     # Exibe uma tabela de dados na página
     st.write("Aqui está uma tabela de dados:")
