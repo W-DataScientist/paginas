5d4e5399d6887c6a2bf43a08648a89460a4eb76d
W-DataScientist <165705997+W-DataScientist@users.noreply.github.com>
2025-01-14 20:23:22 -0300

    Update page_3.py

--- page/page_3.py
+++ page/page_3.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd  # Importa pandas para manipulação de dados em forma de DataFrame
+
 
 # Função principal da Página 3
 def main():
@@ -16,15 +16,4 @@ def main():
         Sinta-se à vontade para modificar os dados ou adicionar novas colunas conforme necessário.
     """)
     
-    # Exibe uma tabela de dados na página
-    st.write("Aqui está uma tabela de dados:")
-    
-    # Cria um dicionário com dados para a tabela
-    data = {
-        'Coluna A': [1, 2, 3],   # Dados da Coluna A
-        'Coluna B': [4, 5, 6],   # Dados da Coluna B
-        'Coluna C': [7, 8, 9]    # Dados da Coluna C
-    }
-    
-    df = pd.DataFrame(data)   # Converte o dicionário em um DataFrame do pandas
-    st.table(df)              # Exibe o DataFrame como uma tabela na aplicação Streamlit
+   
